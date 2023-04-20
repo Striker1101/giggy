7030397c78872797f10dbd96b00af492e5b745d5
striker <goodluckodemilin@gmail.com>
2023-04-20 13:48:04 +0100

    finished header and footer 0

--- comps/Footer.jsx
+++ comps/Footer.jsx
@@ -1,7 +1,136 @@
 import React from "react";
-
+import styles from "../styles/Footer.module.css";
+import { EventAvailable } from "@material-ui/icons";
+import Link from "next/link";
 function Footer(props) {
-  return <div>footer</div>;
+  const newly = [
+    {
+      name: "Drilling Rigs",
+      content: [
+        "4000 HP land rig",
+        "3000 HP land rig",
+        "2000 HP AC (new) land rig",
+        "1500 HP AC (new) land rig",
+        "1500 HP Hydraulic (used) land rig",
+        "1200 HP Fast Moving (refurbished) rig",
+        "1000 HP Super Single (used)",
+      ],
+    },
+    {
+      name: "Top Drives",
+      content: [
+        "GDM 650 1200 HP",
+        "TDS-10, -10SA, -11, -11SA (refurbished)",
+        "TDS-4S  (2)",
+      ],
+    },
+    {
+      nmae: "Mud Pumps",
+      content: ["PZ-9 (used)", "FD-1600", "FC-1600", "F-1000"],
+    },
+    {
+      name: "BOPs",
+      content: [],
+    },
+  ];
+  const available = [
+    {
+      name: "Jackups",
+      content: [
+        "300' CP300, Super M2, DSJ300",
+        "350' KFELS 'B', Super 116 E",
+        "375' BMC, CJ46",
+        "400' JU-2000E, KFELS 'B', CP400, CJ46, KFELS Super 'A'",
+      ],
+    },
+    {
+      name: "Semisubmersibles",
+      content: ["New and Used Available"],
+    },
+    {
+      name: "Drillships",
+      content: ["Ultra Deep Drillships Available"],
+    },
+    {
+      name: "Please complete RFQ for Information",
+      content: [],
+    },
+  ];
+  return (
+    <div>
+      <ul className={styles.cover}>
+        <li className={styles.li}>
+          <div className={styles.header}>
+            <h3>Newly Available Equipment</h3>
+            <hr />
+          </div>
+          {newly.map((item, i) => {
+            return (
+              <>
+                <h4 key={i}>{item.name}</h4>
+                <div>
+                  {item.content.map((item, i) => {
+                    return <p key={i}>{item}</p>;
+                  })}
+                </div>
+              </>
+            );
+          })}
+        </li>
+        <li className={styles.li}>
+          <div className={styles.header}>
+            <h3>Available Offshore Rigs</h3>
+            <hr />
+          </div>
+          {available.map((item, i) => {
+            return (
+              <>
+                <h4 key={i}>{item.name}</h4>
+                <div>
+                  {item.content.map((item, i) => {
+                    return <p key={i}>{item}</p>;
+                  })}
+                </div>
+              </>
+            );
+          })}
+        </li>
+        <li className={styles.li}>
+          <div className={styles.header}>
+            <h3>Contact Us</h3>
+            <hr />
+          </div>
+          <h3>
+            Oil Rigs Now, LLC <br />
+            Houston, Texas <br />
+            713-570-6450 <br />
+            info@giggy.com <br />
+            Request for Quote
+          </h3>
+        </li>
+      </ul>
+      <div class={styles.footer}>
+        <Link href="#" rel="nofollow">
+          Return to top of page
+        </Link>
+
+        <p>
+          Copyright &#x000A9;&nbsp;2023 &#x000B7;
+          <Link href="/">Genesis Framework</Link>
+          &#x000B7;
+        </p>
+      </div>
+      <center className={styles.certs}>
+        <b>
+          Copyright 2011-2018 Oil Rigs Now, LLC - Houston, TX | 713-570-6450 |
+          info@OilRigsNow.com
+        </b>{" "}
+        | <Link href="/certs/legal-notices/">Legal Notices</Link> |{" "}
+        <Link href="/certs/terms-and-conditions/">Terms and Conditions</Link> |{" "}
+        <Link href="/certs/privacy-policy/">Privacy Policy</Link>
+      </center>
+    </div>
+  );
 }
 
 export default Footer;
--- comps/Nav.jsx
+++ comps/Nav.jsx
@@ -1,11 +1,57 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
+import styles from "@/styles/Nav.module.css";
+import classNames from "classnames";
+import { useRef } from "react";
+import { useEffect } from "react";
 function Nav(props) {
+  const land_rig = useRef([1000, 1200, 1300, 1500, 2000, 3000]);
+  const offshore_rig = useRef([
+    {
+      name: "Barge Rigs",
+      arrs: [],
+    },
+    {
+      name: "200 ft. Jack-ups",
+      arrs: [],
+    },
+    {
+      name: "300 ft. Jack-ups",
+      arrs: ["CP-300", "DSJ-300", "Super M2"],
+    },
+    {
+      name: "350 ft. Jack-ups",
+      arrs: ["CJ46"],
+    },
+    {
+      name: "400 ft. Jack-ups",
+      arrs: ["JU-2000E0", "CP-400"],
+    },
+    {
+      name: "Semi-Submersibles",
+      arrs: [],
+    },
+  ]);
+  useEffect(() => {
+    const fold = [...document.querySelectorAll(".fold")];
+    fold.forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        item.style.cssText = "background-color: white;color: black";
+        item.children[0].style.cssText =
+          "display:flex; background-color: white;";
+      });
+      item.addEventListener("mouseleave", () => {
+        item.style.cssText = "background-color: rgb(48, 45, 45);color: wheat";
+        item.children[0].style.cssText =
+          "display:none; background-color: rgb(48, 45, 45)";
+      });
+    });
+  }, []);
   return (
-    <div>
-      <div className="header">
-        <div className="image">
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Link href={"/"} className={styles.image}>
           <Image
             style={{
               layout: "fill",
@@ -18,10 +64,10 @@ function Nav(props) {
             height={150}
             width={55}
           />
-          <h1 className="iconText">GIG</h1>
-        </div>
-        <h3 className="text">
-          Land Drilling Rigs
+          <h1>GIG</h1>
+        </Link>
+        <h3 className={styles.text}>
+          Land DrilLinkng Rigs
           <br />
           Offshore Rigs & Vessels
           <br />
@@ -30,6 +76,79 @@ function Nav(props) {
           713-570-6450
         </h3>
       </div>
+      <nav className={styles.nav}>
+        <ul className={styles.mainUL}>
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li className="fold">
+            About Us
+            <ul className={styles.sub0}>
+              <Link href={"/about"}>About Us</Link>
+              <Link href={"/about/consultant"}>Consultant Fees</Link>
+              <Link href={"/about/finance"}> Financing</Link>
+            </ul>
+          </li>
+          <li className="fold">
+            Land Rigs
+            <ul className={styles.sub0}>
+              {land_rig.current.map((item, index) => {
+                return (
+                  <Link key={index} href={"/land_rig/" + item}>
+                    {item} hp
+                  </Link>
+                );
+              })}
+            </ul>
+          </li>
+          <li className="fold">
+            Mobile Rig
+            <ul className={styles.sub0}>
+              <Link href={"/mobile_rig/mobile rig for sale"}>
+                Mobile Workover Drilling Rigs for Sale
+              </Link>
+              <Link href={"/mobile_rig/cabot_750"}> Cabot 750</Link>
+            </ul>
+          </li>
+          <li className="fold">
+            Offshore Rigs
+            <ul className={styles.sub0}>
+              {offshore_rig.current.map((item, i) => {
+                return (
+                  <Link
+                    key={i}
+                    href={"/offshore_rig/" + item.name}
+                    className="fold"
+                  >
+                    {item.name}
+                    <ul className={styles.sub1}>
+                      {item.arrs.map((sub, i) => {
+                        return (
+                          <Link
+                            key={i}
+                            href={"/offshore_rig/" + item.name + sub}
+                          >
+                            {sub}
+                          </Link>
+                        );
+                      })}
+                    </ul>
+                  </Link>
+                );
+              })}
+            </ul>
+          </li>
+          <li className="fold">
+            Equipment
+            <ul className={styles.sub0}>
+              <Link href={"/Equipment/nov-bop-specials"}>nov-bop-specials</Link>
+            </ul>
+          </li>
+          <li>
+            <Link href={"/quotes"}> Quotes</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
